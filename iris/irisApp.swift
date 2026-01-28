72cf471d872181111d535621a5b5cab3689c4279
tal <tal@tals-MacBook-Pro.local>
2026-01-28 09:31:49 -1000

    Initial Commit

---
+++ iris/irisApp.swift
@@ -0,0 +1,17 @@
+//
+//  irisApp.swift
+//  iris
+//
+//  Created by tal on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct irisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
